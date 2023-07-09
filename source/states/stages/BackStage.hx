588cd6a983f1f44317fae84f6d4f592483511629
StaleTide <brocknewman2006@gmail.com>
2023-07-09 01:44:17 -0500

    a

---
+++ source/states/stages/BackStage.hx
@@ -0,0 +1,99 @@
+package states.stages;
+
+import states.stages.objects.*;
+
+class BackStage extends BaseStage
+{
+	// If you're moving your stage from PlayState to a stage file,
+	// you might have to rename some variables if they're missing, for example: camZooming -> game.camZooming
+
+	override function create()
+	{
+		// Spawn your stage sprites here.
+		// Characters are not ready yet on this function, so you can't add things above them yet.
+		// Use createPost() if that's what you want to do.
+		var construction:BGSprite = new BGSprite('backStage',-500,-80,.9,.9);
+		construction.setGraphicSize(Std.int(0.5 * ruins.width));
+		construction.updateHitbox();
+		add(construction);
+	}
+	
+	override function createPost()
+	{
+		// Use this function to layer things above characters!
+	}
+
+	override function update(elapsed:Float)
+	{
+		// Code here
+	}
+
+	
+	override function countdownTick(count:BaseStage.Countdown, num:Int)
+	{
+		switch(count)
+		{
+			case THREE: //num 0
+			case TWO: //num 1
+			case ONE: //num 2
+			case GO: //num 3
+			case START: //num 4
+		}
+	}
+
+	// Steps, Beats and Sections:
+	//    curStep, curDecStep
+	//    curBeat, curDecBeat
+	//    curSection
+	override function stepHit()
+	{
+		// Code here
+	}
+	override function beatHit()
+	{
+		// Code here
+	}
+	override function sectionHit()
+	{
+		// Code here
+	}
+
+	// Substates for pausing/resuming tweens and timers
+	override function closeSubState()
+	{
+		if(paused)
+		{
+			//timer.active = true;
+			//tween.active = true;
+		}
+	}
+
+	override function openSubState(SubState:flixel.FlxSubState)
+	{
+		if(paused)
+		{
+			//timer.active = false;
+			//tween.active = false;
+		}
+	}
+
+	// For events
+	override function eventCalled(eventName:String, value1:String, value2:String, flValue1:Null<Float>, flValue2:Null<Float>, strumTime:Float)
+	{
+		switch(eventName)
+		{
+			case "My Event":
+		}
+	}
+	override function eventPushed(event:objects.Note.EventNote)
+	{
+		// used for preloading assets used on events
+		switch(event.event)
+		{
+			case "My Event":
+				//precacheImage('myImage') //preloads images/myImage.png
+				//precacheSound('mySound') //preloads sounds/mySound.ogg
+				//precacheMusic('myMusic') //preloads music/myMusic.ogg
+		}
+	}
+}
